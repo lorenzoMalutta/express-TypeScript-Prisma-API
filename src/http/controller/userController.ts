39869e632db7e9336bbabdf8436f8aad84cb71df
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2023-05-17 17:43:34 -0300

    Update userController.ts

--- src/http/controller/userController.ts
+++ src/http/controller/userController.ts
@@ -1,58 +1,70 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
-import User from '../interface/IUser';
+import User from '../../interface/IUser';
 const prisma = new PrismaClient();
 
 export const getUsers = async (req: Request, res: Response) => {
-    const users = await prisma.user.findMany();
-    res.json(users);
+    try {
+        const users = await prisma.user.findMany();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(400).json({ sucess: false });
+    } finally {
+        await prisma.$disconnect();
+    }
 }
 
 export const getUser = async (req: Request, res: Response) => {
     const { id } = req.params;
-    const user = await prisma.user.findUnique({
-        where: {
-            id: parseInt(id)
-        }
-    });
-    res.json(user);
-}
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: parseInt(id)
+            }
+        });
+        res.status(200).json({ sucess: true, user });
+    } catch (error) {
+        res.status(400).json({ sucess: false });
+    } finally {
+        await prisma.$disconnect();
+    }
 
-export const createUser = async (req: Request, res: Response) => {
-    const { name, email, cpf, password }: User = req.body;
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-            cpf,
-            password
-        }
-    });
-    res.status(200).json({ sucess: true });
 }
 
 export const deleteUser = async (req: Request, res: Response) => {
     const { id } = req.params;
-    const user = await prisma.user.delete({
-        where: {
-            id: parseInt(id)
-        }
-    });
-    res.json(user);
+    try {
+        const user = await prisma.user.delete({
+            where: {
+                id: parseInt(id)
+            }
+        });
+        res.status(201).json({ sucess: true, user });
+    } catch (error) {
+        res.status(400).json({ sucess: false });
+    } finally {
+        await prisma.$disconnect();
+    }
 }
 
 export const updateUser = async (req: Request, res: Response) => {
     const { id } = req.params;
-    const { name, email, cpf }: User = req.body;
-    const user = await prisma.user.update({
-        where: {
-            id: parseInt(id)
-        },
-        data: {
-            name,
-            email,
-            cpf
-        }
-    });
-    res.json(user);
+    try {
+        const { name, email, cpf }: User = req.body;
+        const user = await prisma.user.update({
+            where: {
+                id: parseInt(id)
+            },
+            data: {
+                name,
+                email,
+                cpf
+            }
+        });
+        res.json({ sucess: true, user });
+    } catch (error) {
+        res.status(400).json({ sucess: false });
+    } finally {
+        await prisma.$disconnect();
+    }
 }
